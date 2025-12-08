f3609380ebf9944a070a0664bf2785608238b9ea
IzumiNaru <luthfibintang57@gmail.com>
2025-12-08 15:24:29 +0700

    Membuat File Penumpang.java beserta constructor yang di perlukan

---
+++ Penumpang.java
@@ -0,0 +1,49 @@
+package UAS;
+
+public class Penumpang {
+    private String nama; // Ditambahkan untuk keperluan simulasi & method turunkanPenumpang
+    private int id;
+    private int umur;
+    private boolean hamil;
+    private int saldo;
+
+    // Constructor
+    public Penumpang(String nama, int id, int umur, boolean hamil) {
+        this.nama = nama;
+        this.id = id;
+        this.umur = umur;
+        this.hamil = hamil;
+        this.saldo = 10000; // Saldo awal default 10.000
+    }
+
+    // Accessor Methods (Getters)
+    public String getNama() {
+        return nama;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public boolean getHamil() {
+        return hamil;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    // Method untuk menambah saldo
+    public void tambahSaldo(int saldoBaru) {
+        this.saldo += saldoBaru;
+    }
+
+    // Method untuk mengurangi saldo saat bayar ongkos
+    public void kurangiSaldo(int ongkos) {
+        this.saldo -= ongkos;
+    }
+}
